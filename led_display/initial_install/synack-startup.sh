5315930f56cfe961a8bf0c17f92c35a75c3b751e
Malcolm Stagg <malcolmst@gmail.com>
2021-06-14 20:14:14 -0500

    Minor updates to install scripts

--- led_display/initial_install/synack-startup.sh
+++ led_display/initial_install/synack-startup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #
-# This script will be copied into your home directory as ~/led-display-startup.sh.
+# This script will be copied into your home directory as 
+# '~/led-display-startup.sh'.
 # Any modifications can be made to that file as desired. 
 #
-# The original is stored in the `initial_install` directory, and should generally
-# not be modified. It can be be restored by running the `synack-restore-defaults.sh`
-# script.
+# The original is stored in the `initial_install` directory, and should
+# generally not be modified. It can be be restored by running the
+# '~/led-display-restore-defaults.sh' script.
 
 sudo LED_DISPLAY_CONFIG=/home/pi/synack_config python -m led_display
 
